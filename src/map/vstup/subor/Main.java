cde8776528ebf206f695f95e2935ba72ac948b68
Roman Ravas <romorko@gmail.com>
2020-03-13 21:26:13 +0100

    BufferedReader

--- src/map/vstup/subor/Main.java
+++ src/map/vstup/subor/Main.java
@@ -1,8 +1,33 @@
 package map.vstup.subor;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("data.in"));
+            String st;
+            while ((st = br.readLine()) != null)
+            {
+                System.out.println(st);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Subor sa nepodarilo otvorit!");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Chyba pri citani dat!");
+        }
+
     }
 }
+
